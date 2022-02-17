a036df42bfc314008bf440dd4359528f65c1a75a
Smita1706 <smitachinchkar1998@gmail.com>
2022-02-17 20:26:00 +0530

    Addded : The MyLinkedList.java file containing all the methods of linked list is added

---
+++ main/java/com/bridgelabz/stackandqueue/MyLinkedList.java
@@ -0,0 +1,103 @@
+package com.bridgelabz.stackandqueue;
+
+public class MyLinkedList {
+	public INode head;
+	public INode tail;
+	public MyLinkedList() {
+		this.head = null;
+		this.tail = null;
+	}
+
+	public void add(INode newNode ) {
+		if(this.head == null)
+			this.head = newNode;
+		if(this.tail == null)
+			this.tail = newNode;
+		else {
+			INode tempNode = this.head;
+			this.head = newNode;
+			this.head.setNext(tempNode);
+		}
+	}
+
+	public void apend(INode newNode) {
+		if(this.head == null)
+			this.head = newNode;
+		if(this.tail == null)
+			this.tail = newNode;
+		else {
+			this.tail.setNext(newNode);
+			this.tail = newNode;
+		}
+	}
+
+	public void Insert(INode myFirstNode,INode newNode) {
+		INode tempNode = myFirstNode.getNext();
+		myFirstNode.setNext(newNode);
+		newNode.setNext(tempNode);
+	}
+
+	public INode pop() {
+		INode tempNode = this.head;
+		this.head = head.getNext();
+		return  tempNode;
+	}
+
+	public INode popLast() {
+		INode secondLastNode = this.head;
+		while(secondLastNode.getNext().getNext() != null)
+			secondLastNode = secondLastNode.getNext();
+		secondLastNode.setNext(null);
+		return this.head;
+	}
+	
+	public boolean search(INode node) {
+		INode tempNode = head;
+		boolean found = false;
+		while(tempNode.getNext() != null) {
+			if(tempNode.getKey() == node.getKey()) {
+				found = true;
+			}
+			tempNode = tempNode.getNext();
+		}
+		return found;
+	}
+
+	public void searchAndInsert(INode searchNode ,INode insertNode) {
+		MyLinkedList n = new MyLinkedList();
+		INode tempNode = head;
+		boolean inserted = false;
+		while(tempNode.getNext() != null) {
+			if(tempNode.getKey() == searchNode.getKey()) {
+				n.Insert(searchNode,insertNode);
+			}
+			tempNode = tempNode.getNext();
+		}
+	}
+	
+	public void searchAndDelete(INode deleteNode) {
+		MyLinkedList n = new MyLinkedList();
+		INode tempNode = head;
+		boolean inserted = false;
+		while(tempNode.getNext() != null) {
+			if(tempNode.getKey() == deleteNode.getKey()) {
+				n.pop();
+			}
+			tempNode = tempNode.getNext();
+		}
+	}
+	
+	public void printMyNodes() {
+
+		StringBuffer myNodes = new StringBuffer ("My Nodes: "); 
+		INode tempNode = head;
+		while (tempNode.getNext() != null)
+		{
+			myNodes.append(tempNode.getKey());
+			if(!tempNode.equals (tail)) myNodes.append("->"); 
+			tempNode = tempNode.getNext();
+		}
+		myNodes.append(tempNode.getKey());
+		System.out.println (myNodes);
+	}
+}
